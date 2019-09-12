937b9095d61d8b056ccbd91eac9e7d460f297295
Miguel Martínez Serrano <miguelms.ovi@gmail.com>
2019-09-12 03:21:19 +0200

    navigation layout, jquery yarn, boostrap added gemfile fix.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+
+
   resources :projects
   resources :tasks
   resources :companies
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: "index#index"
 end
